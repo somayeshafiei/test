0e492da4c7a804223b39c805d6eb37ff59850b5d
Somaye Shafiei <S.shafiee76@gmail.com>
2023-01-19 15:01:27 +0330

    first commit

---
+++ indx.js
@@ -0,0 +1,20 @@
+// function reversing(input) {
+//   input = input.split(" ");
+//   tempt = "";
+//   for (i = input.length - 1; i >= 0; i--) {
+//     tempt += input[i] + " ";
+//   }
+//   console.log(tempt);
+// }
+// reversing("I Am from Iran and I attended the maktab sharif class.");
+
+// let result = [];
+// let curr = "";
+// while (true) {
+//   curr = prompt("number : ");
+//   if (Number(curr) === 0) break;
+//   arr.push(Number(curr));
+// }
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
